5f83283c611d4102168b475e58f187ec1c3fe217
fxss <723107234@qq.com>
2022-01-30 11:36:13 +0800

    feat: 新增防抖节流函数

---
+++ src/debounce.js
@@ -0,0 +1,34 @@
+/**
+ * 防抖函数
+ * @param {Function} fn 是需要防抖处理的函数
+ * @param {Number} wait 是时间间隔
+ * @param {Boolean} immediate 表示第一次是否立即执行
+ * @returns 
+ */
+function debounce(fn, wait = 50, immediate) {
+  // 通过闭包缓存一个定时器 id
+  let timer = null
+  // 将 debounce 处理结果当作函数返回
+  // 触发事件回调时执行这个返回函数
+  return function (...args) {
+    // 如果已经设定过定时器就清空上一次的定时器
+    if (timer) clearTimeout(timer)
+
+    // immediate 为 true 表示第一次触发后执行
+    // timer 为空表示首次触发
+    if (immediate && !timer) {
+      fn.apply(this, args)
+    }
+
+    // 开始设定一个新的定时器，定时器结束后执行传入的函数 fn
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, wait)
+  }
+}
+
+// DEMO
+// 执行 debounce 函数返回新函数
+const betterFn = debounce(() => console.log('fn 防抖执行了'), 1000, true)
+// 第一次触发 scroll 执行一次 fn，后续只有在停止滑动 1 秒后才执行函数 fn
+document.addEventListener('scroll', betterFn)
---
+++ src/throttle.js
@@ -0,0 +1,21 @@
+/**
+ * 节流函数
+ * @param {Function} fn 是需要执行的函数
+ * @param {Number} wait 是时间间隔
+ * @returns 
+ */
+const throttle = (fn, wait = 50) => {
+  // 上一次执行 fn 的时间
+  let previous = 0
+  // 将 throttle 处理结果当作函数返回
+  return function (...args) {
+    // 获取当前时间，转换成时间戳，单位毫秒
+    let now = +new Date()
+    // 将当前时间和上一次执行函数的时间进行对比
+    // 大于等待时间就把 previous 设置为当前时间并执行函数 fn
+    if (now - previous > wait) {
+      previous = now
+      fn.apply(this, args)
+    }
+  }
+}
