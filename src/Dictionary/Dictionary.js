2a7be0ed8ef7b90d35e1ff5bc507ea38f424012e
fxss <723107234@qq.com>
2022-02-05 14:29:02 +0800

    feat: 字典的简单实现及示例

---
+++ src/Dictionary/Dictionary.js
@@ -0,0 +1,45 @@
+class Dictionary {
+  constructor() {
+    this.dataStore = {}
+  }
+
+  add(key, value) {
+    this.dataStore[key] = value
+  }
+
+  find(key) {
+    return this.dataStore[key]
+  }
+
+  remove(key) {
+    delete this.dataStore[key]
+  }
+
+  getKeys() {
+    return Object.keys(this.dataStore)
+  }
+
+  getKeysSort() {
+    return this.getKeys().sort()
+  }
+
+  count() {
+    return this.getKeys().length
+  }
+
+  showAll() {
+    let res = ''
+    this.getKeys().forEach(key => {
+      res += `,{key:'${key}',value:'${this.dataStore[key]}'}`
+    })
+    return res.slice(1)
+  }
+
+  showAllSort() {
+    let res = ''
+    this.getKeys().sort().forEach(key => {
+      res += `,{key:'${key}',value:'${this.dataStore[key]}'}`
+    })
+    return res.slice(1)
+  }
+}
---
+++ src/Dictionary/README.md
@@ -0,0 +1,16 @@
+# 字典
+
+字典是一种以 键值对 形式存数数据的数据结构，`javaScript` 中的 `Object` 类就是以字典的形式设置的，所以使用 `Object` 类本身的特性实现字典 `Dictionary` 类。
+
+## 属性及方法
+
+| 列表 | 属性或方法 | 描述 |
+|----|----|----|
+| add | 方法 | `add(key, value)` 向字典中添加 键值对 |
+| find | 方法 | `find(key)` 从字典中查找 `key` 键对应的值，找不到则返回 `undefined` |
+| remove | 方法 | `remove(key)` 从字典中删除 `key` 键 |
+| getKeys | 方法 | `getKeys()` 获取字典中的所有 `key` |
+| getKeysSort | 方法 | `getKeysSort()` 获取字典中的所有 `key`，并使用 `sort` 排序 |
+| count | 方法 | `count()` 返回字典中 键值对 的个数 |
+| showAll | 方法 | `showAll()` 将字典中的所有 键值对 转换为字符串，以 `,` 分隔 |
+| showAllSort | 方法 | `showAllSort()` 将字典中的所有键按照 `sort` 排序，之后将所有 键值对 转换为字符串，以 `,` 分隔 |
